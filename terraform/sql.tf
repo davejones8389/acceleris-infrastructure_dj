6ad9114f035fc70a414dac123e26995c3d2bfab7
Dave Jones <dave.jones@kpmg.co.uk>
2023-01-30 14:17:57 +0000

    push changes

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -1,5 +1,5 @@
 resource "azurerm_mssql_server" "acceleris_server" {
-  name                         = "${var.project_name}${var.environment}sql"
+  name                         = "${var.project_name}${var.environment}sql9123"
   resource_group_name          = azurerm_resource_group.resource_group.name
   location                     = var.location
   version                      = "12.0"
